8ef411dc998b9f357de3bd298f79063595e75b70
JonathanArns <jonathan.arns@googlemail.com>
2021-01-31 15:42:40 +0100

    add kafka-connect and flink images

---
+++ docker-images/kafka-connect/deploy-connectors.sh
@@ -0,0 +1,45 @@
+sleep 1m
+
+curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d '{
+        "name": "tweet_data_source",
+        "config": {
+                "tasks.max": "1",
+                "connector.class": "com.github.jcustenborder.kafka.connect.spooldir.SpoolDirCsvSourceConnector",
+                "input.path": "/home/data",
+                "error.path": "/home/data/error",
+                "finished.path": "/home/data/finished",
+                "halt.on.error": "false",
+                "errors.tolerance": "all",
+                "errors.deadletterqueue.topic.name": "tweet_deadletterqueue",
+                "errors.deadletterqueue.topic.replication.factor": "1",
+                "empty.poll.wait.ms": "3000",
+                "csv.first.row.as.header": "true",
+                "schema.generation.enabled": "true",
+                "csv.null.field.indicator": "EMPTY_SEPARATORS",
+                "csv.separator.char": "44",
+                "input.file.pattern": ".*?tweets.*?\\.csv",
+                "topic": "tweets"
+                }
+        }'
+
+curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d '{
+        "name": "follower_data_source",
+        "config": {
+                "tasks.max": "1",
+                "connector.class": "com.github.jcustenborder.kafka.connect.spooldir.SpoolDirCsvSourceConnector",
+                "input.path": "/home/data",
+                "error.path": "/home/data/error",
+                "finished.path": "/home/data/finished",
+                "halt.on.error": "false",
+                "errors.tolerance": "all",
+                "errors.deadletterqueue.topic.name": "follower_deadletterqueue",
+                "errors.deadletterqueue.topic.replication.factor": "1",
+                "empty.poll.wait.ms": "3000",
+                "csv.first.row.as.header": "true",
+                "schema.generation.enabled": "true",
+                "csv.null.field.indicator": "EMPTY_SEPARATORS",
+                "csv.separator.char": "32",
+                "input.file.pattern": ".*?combine.*?\\.txt",
+                "topic": "follows"
+                }
+        }'
