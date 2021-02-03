a281e458fde51e64ad39338a635ce8b157b86a58
KonstantinGasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2021-02-03 19:49:48 +0100

    neo4j tweeks

--- docker_files/container-stack/kafka-connect/deploy-connectors.sh
+++ docker_files/container-stack/kafka-connect/deploy-connectors.sh
@@ -62,23 +62,40 @@ curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json
        }
 }'
 
-
 curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d '{
-      "name": "Neo4jSinkConnector",
-      "config": {
-              "topics": "follows",
-              "connector.class": "streams.kafka.connect.sink.Neo4jSinkConnector",
-              "errors.retry.timeout": "-1",
-              "errors.retry.delay.max.ms": "1000",
-              "errors.tolerance": "all",
-              "errors.log.enable": true,
-              "errors.deadletterqueue.topic.name": "follower_deadletterqueue",
-              "errors.deadletterqueue.topic.replication.factor": "1",
-              "errors.log.include.messages": true,
-              "neo4j.server.uri": "bolt://neo4j:7687",
-              "neo4j.authentication.basic.username": "neo4j",
-              "neo4j.authentication.basic.password": "sink",
-              "neo4j.encryption.enabled": false,
-              "neo4j.topic.cypher.follows": "MERGE (x:User{id: event.follow_from}) MERGE (y:User{id: event.follow_to}) MERGE (x)-[:FOLLOWS]->(y)"
-      }
+       "name":"mongo-follows",
+       "config" :{
+               "connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
+               "tasks.max":"5",
+               "database":"zwitscher_store",
+               "collection":"follows",
+               "write.batch.enabled":"false",
+               "connect.use_schema":"false",
+               "topics":"follows",
+	             "connection.uri":"mongodb://root:root@mongo:27017/",
+               "key.converter":"org.apache.kafka.connect.json.JsonConverter",
+               "key.converter.schemas.enable":"false",
+               "value.converter":"org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable":"false"
+       }
 }'
+
+# curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d '{
+#       "name": "Neo4jSinkConnector",
+#       "config": {
+#               "topics": "follows",
+#               "connector.class": "streams.kafka.connect.sink.Neo4jSinkConnector",
+#               "errors.retry.timeout": "-1",
+#               "errors.retry.delay.max.ms": "1000",
+#               "errors.tolerance": "all",
+#               "errors.log.enable": true,
+#               "errors.deadletterqueue.topic.name": "follower_deadletterqueue",
+#               "errors.deadletterqueue.topic.replication.factor": "1",
+#               "errors.log.include.messages": true,
+#               "neo4j.server.uri": "bolt://neo4j:7687",
+#               "neo4j.authentication.basic.username": "neo4j",
+#               "neo4j.authentication.basic.password": "sink",
+#               "neo4j.encryption.enabled": false,
+#               "neo4j.topic.cypher.follows": "MERGE (x:User{id: event.follow_from}) MERGE (y:User{id: event.follow_to}) MERGE (x)-[:FOLLOWS]->(y)"
+#       }
+# }'
