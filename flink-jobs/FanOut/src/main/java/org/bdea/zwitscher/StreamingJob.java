352859c09deb573fc9e06388049c86f796e9834a
JonathanArns <jonathan.arns@googlemail.com>
2021-02-03 21:33:17 +0100

    add FanOut Job

---
+++ flink-jobs/FanOut/src/main/java/org/bdea/zwitscher/StreamingJob.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bdea.zwitscher;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.connectors.kafka.internals.KeyedSerializationSchemaWrapper;
+
+import java.util.*;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+
+		ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+		final String jobName = parameterTool.get("job-name", "fan_out_job");
+		final String inputTopic = parameterTool.get("input-topic", "clean_tweets");
+		final String consumerGroup = parameterTool.get("group-id", "zwitscher");
+		final String kafkaAddress = parameterTool.get("kafka-address", "kafka:29092");
+
+		final ObjectMapper objectMapper = new ObjectMapper();
+
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		//create a new kafka consumer
+		Properties consumerProps = new Properties();
+		consumerProps.setProperty("bootstrap.servers", kafkaAddress);
+		consumerProps.setProperty("group.id", consumerGroup);
+		FlinkKafkaConsumer<String> flinkKafkaConsumer = new FlinkKafkaConsumer<>(inputTopic,
+				new SimpleStringSchema(), consumerProps);
+
+		//add the consumer to the environment as a data-source, to get a DataStream
+		DataStream<String> dataStream = env.addSource(flinkKafkaConsumer);
+
+		// parse json and assign a flink event-time timestamp to each element
+		DataStream<ObjectNode> jsonStream = dataStream
+				.map((MapFunction<String, ObjectNode>) value -> (ObjectNode) objectMapper.readTree(value));
+
+		// filter tweets by bad words and add random ids
+		DataStream<ObjectNode> filteredStream = jsonStream
+				.map(value -> {
+					String content = value.get("content").textValue();
+					ObjectNode objNode = new ObjectMapper().createObjectNode();
+					objNode.set("author", value.get("author"));
+					objNode.set("number_of_likes", value.get("number_of_likes"));
+					objNode.set("number_of_shares", value.get("number_of_shares"));
+					if (ProfanityFilter.containsProfanity(content)) {
+//						value.put("content", "[censored]");
+						objNode.put("content", "[censored]");
+						return objNode;
+					}
+					objNode.put("content", value.get("content"));
+					return objNode;
+				}).map(value -> {
+					try {
+						value.get("user_id").intValue();
+					} catch (Exception e) {
+						value.put("user_id", RandomId.get());
+					}
+					return value;
+				});
+
+		// serialize
+		DataStream<String> outputStream = filteredStream.map(value -> objectMapper.writeValueAsString(value));
+
+		//create a new kafka producer
+		Properties producerProps = new Properties();
+		producerProps.setProperty("bootstrap.servers", kafkaAddress);
+		FlinkKafkaProducer<String> flinkKafkaProducer = new FlinkKafkaProducer<>(outputTopic,
+				new KeyedSerializationSchemaWrapper<>(new SimpleStringSchema()),
+				producerProps, FlinkKafkaProducer.Semantic.AT_LEAST_ONCE);
+
+		//add the producer to the dataStream as a sink
+		outputStream.addSink(flinkKafkaProducer);
+
+		// execute program
+		env.execute(jobName);
+	}
+}
